adc06b61c5e2c8d11d5f1a5eba0e10cfc1d40afe
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-09-08 20:21:54 -0500

    Terminando html - mejorando read me - listo

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const btnHome = document.getElementById('btn-home');
+const btnHistory = document.getElementById('btn-history');
+const home = document.getElementById('home');
+const btnCachangas = document.getElementById('btn-cachangas');
+const btnCombos = document.getElementById('btn-combos');
+const ourHistory = document.getElementById('our-history');
+
+
+btnHome.addEventListener('click', () => {
+
+    showSections(event, 'home');
+
+});
+
+btnHistory.addEventListener('click', () => {
+
+    showSections(event, 'our-history');
+
+}); 
+
+btnCombos.addEventListener('click', () => {
+
+    swal('¡Pronto estarán disponibles!');
+    
+})
